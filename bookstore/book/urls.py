b07480398df7701a1e9313db50286caf1b967490
ramknss <154682413+ramknss@users.noreply.github.com>
2023-12-23 19:24:51 +0530

    Add files via upload

---
+++ bookstore/book/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from .views import *
+
+urlpatterns =[
+    path('', home,name='home'),
+    path('login/', loginPage,name='login'),
+    path('viewcart/', viewcart,name='viewcart'),
+    path('addbook/', addbook,name='addbook'),
+    path('register/', registerPage,name='register'),
+    path('logout/', logoutPage,name='logout'),
+    path('addtocart/<str:pk>', addtocart,name='addtocart'),
+    path('updatebook/<int:s>/',updatebook,name='updatebook'),
+    path('deletebook/<int:p>/',deletebook,name='deletebook'),
+]
